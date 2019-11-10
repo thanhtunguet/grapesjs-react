7118ce932cef12ce9c85c1c12abd7490d20d7ba2
Thanh Tùng <ht@thanhtunguet.info>
2019-11-10 08:28:36 +0700

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function compile {
-  ./compile.sh;
+  ./scripts/compile.sh;
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
