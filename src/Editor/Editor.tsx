b4fbdbea9f7d1835e17e2c3297e0a5a370eeba08
Eric <epastuer@outlook.com>
2019-12-19 00:31:44 -0700

    onInit receives current editor

--- src/Editor/Editor.tsx
+++ src/Editor/Editor.tsx
@@ -96,7 +96,7 @@ const Editor = React.forwardRef(
           });
           setEditor(newEditor);
           if (onInit) {
-            onInit(editor);
+            onInit(newEditor);
           }
         }
         return handleCleanup;
