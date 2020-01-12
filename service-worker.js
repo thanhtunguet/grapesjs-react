efb5abf95d2d0d6035e635e35504055c80a4f617
Travis CI <ht@thanhtunguet.info>
2020-01-12 02:26:43 +0000

    Deploy to GitHub Pages: d6182beca6045ea75d646d6bd0899393121592fc

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thanhtunguet/grapesjs-react/wiki/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
