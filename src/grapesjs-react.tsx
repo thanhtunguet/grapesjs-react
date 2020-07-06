db458d988b812079822a4f3bde382fc9221b8d70
tungpt <ht@thanhtunguet.info>
2020-07-07 01:25:47 +0700

    Fix imports

--- src/grapesjs-react.tsx
+++ src/grapesjs-react.tsx
@@ -1,3 +1,10 @@
 import 'source-map-support/register';
+import Editor from './Editor/Editor';
 
-export * from './Editor/Editor';
+export type {EditorProps} from './Editor/Editor';
+
+export {
+  Editor,
+};
+
+export default Editor;
