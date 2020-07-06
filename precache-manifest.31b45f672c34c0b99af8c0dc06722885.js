3d6a1251f4390fb2cc08f0a452c198bdc8617f97
Travis CI <ht@thanhtunguet.info>
2020-07-06 18:08:23 +0000

    Deploy to GitHub Pages: 6a58972350a11d7232ad0fd9bafaa4ff30fafee6

---
+++ precache-manifest.31b45f672c34c0b99af8c0dc06722885.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3c9d02345580b3aabdf439f0bf8621",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b2522b814543ccf5a26a.bundle.js"
+  },
+  {
+    "url": "runtime~main.b2522b814543ccf5a26a.bundle.js"
+  },
+  {
+    "revision": "10e34797c389befa6ba4",
+    "url": "static/css/main.3b84d77a.chunk.css"
+  },
+  {
+    "revision": "7b3b8193c0707f27e638",
+    "url": "static/css/vendors~main.b181ba68.chunk.css"
+  },
+  {
+    "revision": "38b30fa0229248d8bd3a14c802046743",
+    "url": "static/media/main-fonts.38b30fa0.ttf"
+  },
+  {
+    "revision": "638868fe8f33b0b45c29fc4e4fba99a9",
+    "url": "static/media/main-fonts.638868fe.woff"
+  },
+  {
+    "revision": "eceff26a5f33cc81123f437a16a78dbe",
+    "url": "static/media/main-fonts.eceff26a.svg"
+  },
+  {
+    "revision": "f7c56e52c2c44690a0e39d5a486d0c3b",
+    "url": "static/media/main-fonts.f7c56e52.eot"
+  },
+  {
+    "url": "vendors~main.b2522b814543ccf5a26a.bundle.js"
+  },
+  {
+    "url": "vendors~main.b2522b814543ccf5a26a.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.19ab1ce736e8ca735e83bcb3060c9188.js"
+  "precache-manifest.31b45f672c34c0b99af8c0dc06722885.js"
 );
 
 self.addEventListener('message', (event) => {
